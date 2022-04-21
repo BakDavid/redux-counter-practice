6f84e6bd7b22d413976d93391f24c43392c27042
Bak David <bak.david96@gmail.com>
2022-04-21 13:25:02 +0300

    Working with multiple slices

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import Counter from './components/Counter';
-
+import Counter from "./components/Counter";
+import React from "react";
+import { useSelector } from "react-redux";
+import Header from "../src/components/Header";
+import Auth from "../src/components/Auth";
+import UserProfile from "./components/UserProfile";
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
   return (
-    <Counter />
+    <>
+      <Header />
+      {!isAuth ? <Auth /> : <UserProfile />}
+      <Counter />
+    </>
   );
 }
 
